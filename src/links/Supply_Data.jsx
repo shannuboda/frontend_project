427c40cd97e58c6a604ca51f3b3c07f041daa231
shannuboda <shanmukha.boda@gmail.com>
2025-01-23 19:32:05 +0530

    first

--- src/links/Supply_Data.jsx
+++ src/links/Supply_Data.jsx
@@ -2,13 +2,12 @@ import { useState, useEffect } from "react";
 import Header from "./Header";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
 function Supply_Data() {
   const [selectedSemester, setselectedSemester] = useState("");
   const [marksheetData, setMarksheetData] = useState([]);
   const [response, setResponse] = useState(null); // Initialize with null
-  const [GPA, setGPA] = useState();
   // Login related code
   console.log(response);
 
@@ -36,9 +35,8 @@ function Supply_Data() {
     const semData = await axios.get(
       `https://backend-project-1nk6.onrender.com/getsupply/${response.username}/${response.year}/${selectedSemester}/${response.branch}/${response.regulation}`
     );
-   
-    if(semData.data.message==="No Data Found")
-    {
+
+    if (semData.data.message === "No Data Found") {
       toast.error("No Data Found, Please Contact Your Administrator");
     }
     setMarksheetData(semData.data);
@@ -47,7 +45,6 @@ function Supply_Data() {
 
   // Print Mode
   const [isPrintMode, setIsPrintMode] = useState(false);
-
 
   const handleCheckboxChange = (id) => {
     setMarksheetData((prevRows) =>
@@ -62,38 +59,38 @@ function Supply_Data() {
     setTimeout(() => {
       window.print();
       setIsPrintMode(false); // Reset after print
-    }, 0);
+    }, 100);
   };
 
   return (
     <div>
- <Toaster
-  position="top-right"
-  reverseOrder={true}
-  gutter={8}
-  containerClassName=""
-  containerStyle={{}}
-  toastOptions={{
-    // Define default options
-    className: '',
-    duration: 5000,
-    removeDelay: 1000,
-    style: {
-      background: 'black',
-      color: 'white',
-    },
+      <Toaster
+        position="top-right"
+        reverseOrder={true}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: "",
+          duration: 5000,
+          removeDelay: 1000,
+          style: {
+            background: "black",
+            color: "white",
+          },
 
-    // Default options for specific types
-    success: {
-      duration: 3000,
-      iconTheme: {
-        primary: 'green',
-        secondary: 'white',
-      },
-    },
-  }}
-/>
-      
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: "green",
+              secondary: "white",
+            },
+          },
+        }}
+      />
+
       <Header studentData={studentData}></Header>
 
       <div className="marksheet-container">
@@ -141,9 +138,6 @@ function Supply_Data() {
                 <h3>Regulation: {response.regulation}</h3>
                 <h3>Batch: {response.year}</h3>
               </div>
-              <div className="stu_intro_2">
-                <h3>SGPA: {GPA}</h3>
-              </div>
             </div>
 
             <table className="marksheet-table">
@@ -157,26 +151,34 @@ function Supply_Data() {
                 </tr>
               </thead>
               {
- <tbody>
- 
-  
- {marksheetData.filter((student) => !isPrintMode || student.checked).map((student, index) => (
-   
-   <tr key={index}>
-     <td><input type="checkbox" name="" id={index+1} checked={student.checked} onChange={() => handleCheckboxChange(student.id)}/></td>
-     <td>{student.name}</td>
-     <td>{student.status}</td>
-     <td>{student.credits}</td>
-     <td>{student.grade}</td>
-   </tr>
- ))}
-</tbody>
+                <tbody>
+                  {marksheetData
+                    .filter((student) => !isPrintMode || student.checked)
+                    .map((student, index) => (
+                      <tr key={index} data-checked={student.checked}>
+                        <td>
+                          <input
+                            type="checkbox"
+                            name=""
+                            id={index + 1}
+                            checked={student.checked}
+                            onChange={() => handleCheckboxChange(student.id)}
+                          />
+                        </td>
+                        <td>{student.name}</td>
+                        <td>{student.status}</td>
+                        <td>{student.credits}</td>
+                        <td>{student.grade}</td>
+                      </tr>
+                    ))}
+                </tbody>
               }
-             
             </table>
           </div>
         )}
-        <button type="button" onClick={()=>handlePrint()}>Print</button>
+        <button type="button" onClick={() => handlePrint()}>
+          Print
+        </button>
       </div>
     </div>
   );
